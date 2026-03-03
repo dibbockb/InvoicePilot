7e1e421ed68804bc413a83ee1cc2960609af62c2
Dibbo <divyajitchakraborty@gmail.com>
2026-03-04 00:42:59 +0600

    Added Hero Section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Hero from './Components/UI/Hero'
 import Navbar from './Components/UI/Navbar'
 
 function App() {
@@ -6,6 +7,9 @@ function App() {
   return (
     <div>
       <Navbar></Navbar>
+      <br />
+      <br />
+      <Hero></Hero>
 
     </div>
   )
